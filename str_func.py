9d2bb3ba2b70cf68486b56c593cce635c867ffd1
Stas <stasfuryus@gmail.com>
2023-12-23 07:19:44 +0300

    add up_first_letter in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def up_string(value: str):
-    return value.upper
+    """делает все буквы заглавными"""
+    return value.upper()
+
+
+def up_first_letter(value):
+    """делает первую букву в каждом слове заглавной"""
+    return value.title()
